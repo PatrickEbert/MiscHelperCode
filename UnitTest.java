3059914fe8f6701ac93186c64650054d17f8b9d5
PatrickEbert <p.ebert16@web.de>
2015-08-03 09:44:08 +0200

    Brush Up
    Brush Up

--- UnitTest.java
+++ UnitTest.java
@@ -2,7 +2,7 @@ public class UnitTest
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Willkommen zum UnitTest");
+		System.out.println("Welcome to UnitTesting!");
 		/*CLASS GEOLOCATION DISTANCE CALCULATION
 		System.out.println("C L A S S  G E O L O C A T I O N  D I S T A N C E  C A L C U L A T I O N");
 		System.out.println("Distanz Lachendorf (52.614549;10.23677) - Zurich (47.3844448;8.509779): " + Geolocation.calculateDistance(52.61454,10.23677,47.3844448,8.509779));
@@ -29,9 +29,12 @@ public class UnitTest
 		Misc.log("Hallo Welt!");
 		Misc.log("Hallo Welt!","LogBook.lg");
 		*/
+		/*WGS84 (LONGITUDE AND LATITUDE) CONVERTED TO CH1903 (SWISS MILITARY COORDINATES)
 		Geolocation g1 = new Geolocation(8.5097799, 47.3844448);
 		System.out.println("Point (lat 47.3844448, lng 8.5097799): " + g1.getCH1903XValue() + ":" + g1.getCH1903YValue());
 		Geolocation g2 = new Geolocation(7.029184, 47.223611);
 		System.out.println("Point (lat 47.223611, lng 7.029184): " + g2.getCH1903XValue() + ":" + g2.getCH1903YValue());
+		*/
+		
 	}
 }
