1c711af892e47bd0536f2dc81827b5496f1b1856
PatrickEbert <p.ebert16@web.de>
2015-08-05 09:50:55 +0200

    My commit
    any commit

--- Misc.java
+++ Misc.java
@@ -1,7 +1,16 @@
 import java.io.*;
-
+/**
+*
+* Class Misc represents a collection of simple static methods which are used across the board to simplify the code and remove necessary but offtopic code from "business" logic
+*
+*/
 public class Misc
 {
+	/**
+	* log(String text) writes any given text to a file called 'Log.txt' in the actual working directory. If 'Log.txt' doesn't exist it will be created, if it exists the text wîll be appended. At the end a newline character will be appended
+	*
+	* @param text given by caller to write into a './Log.txt' ended by a newline character
+	*/
 	public static void log(String text)
 	{
 		try
@@ -15,6 +24,13 @@ public class Misc
 		}
 	}
 	
+	/**
+	* log(String text) writes any given text to a file addressed by the file parameter in the actual working directory. If the file doesn't exist it will be created, if it exists the text wîll be appended. At the end a newline character will be appended
+	*
+	* @param text given by caller to write into a './[file]' ended by a newline character
+	*
+	* @param file the file the text should be logged in
+	*/
 	public static void log(String text, String file)
 	{
 		try
